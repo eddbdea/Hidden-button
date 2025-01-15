1f5f57e65a13b38b529d51d0fe802b55fdca94b7
Eduard Badea <eduardbadea@outlook.com>
2025-01-15 15:02:44 +0200

    Update

---
+++ script.js
@@ -0,0 +1,14 @@
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const winner = getRandomInt(1, 3)
+
+function isWinner(button) {
+    const buttonNumber = parseInt(button.getAttribute('data-number'))
+    if (buttonNumber == winner) {
+        alert("Congratulations! You won!")
+    } else {
+        alert("You lost")
+    }
+}
